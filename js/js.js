4fcb7e8aaa57fbe8e29de7dffb095ad14c2b8a30
Abdelrazek Emad <53626803+AbdelrazekEmad@users.noreply.github.com>
2022-12-10 01:21:53 +0200

    upload

--- js/js.js
+++ js/js.js
@@ -120,3 +120,21 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+
+let tabs = document.querySelectorAll(".features__tabs");
+let tabsArray = Array.from(tabs);
+let contentBlocks = document.querySelectorAll(".features__info");
+let contentBlocksArray = Array.from(contentBlocks);
+
+tabsArray.forEach((ele) => {
+  ele.addEventListener("click", function (e) {
+    tabsArray.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+    contentBlocksArray.forEach((block) => {
+      block.style.display = "none";
+    });
+    document.querySelector(e.currentTarget.dataset.btn).style.display = "block";
+  });
+});
